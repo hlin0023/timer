a3463954f7eda7a3f073fc285a05aa5a2c3f1a9c
hlin0023 <hlin0023>
2024-11-29 14:13:33 +1100

    Timer countdown and time covert functions

--- WorkoutTimer/ViewController.swift
+++ WorkoutTimer/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var timeCount: UILabel!
     var timer :Timer?
     var counter = 0 // count for timer
 
@@ -19,20 +20,40 @@ class ViewController: UIViewController {
 
 
     @IBAction func start(_ sender: Any) {
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        if timer == nil{
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        }
+//        else{}
     }
     
     @objc func fireTimer() {
         counter += 1
         print("Timer fired!", counter)
+        timeCount.text = secondsToHoursMinutesSeconds(counter)
+        
     }
     
     @IBAction func killTimer(_ sender: Any) {
+        displayMessage(title: "End", msg: "Your timer will be stoped ")
+        
         timer?.invalidate()
         timer = nil
         
-        print("This is second \(counter) s")
+        print("This is second \(secondsToHoursMinutesSeconds(counter)) s")
         counter = 0
+//        print(secondsToHoursMinutesSeconds(100000))
+    }
+    
+    // pop up windows display message
+    func displayMessage(title: String, msg: String) {
+        let alertController = UIAlertController(title: title, message: msg, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+
+    func secondsToHoursMinutesSeconds(_ seconds: Int) -> String {
+        return "\(seconds / 3600):\((seconds % 3600) / 60):\((seconds % 3600) % 60)"
     }
     
 }
